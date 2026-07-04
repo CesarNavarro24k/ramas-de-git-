46a2df4601901de8902dfcd2768bd3d396d2bb4f
Cesar <cesarnavarro@gmail.com>
2026-07-04 09:10:22 -0500

    segundo

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import random
 numero = int(input("Cual crees que es el numero entre 1 y 10:"))
 
 numero2 = random.randint(1,10)
-
+puntaje = 0
 if numero == numero2:
     print("Has adivinado el numero")
+    puntaje += 1
+    print(f"Has adivinado el numero, tienes {puntaje} puntos")
 else:
     print(f"No has adivinado el numero, el numero era {numero2}")
